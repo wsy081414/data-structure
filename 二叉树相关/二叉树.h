51b389727e630f6c7968565861e8006f14edca52
yuzhe <953659912@qq.com>
2016-11-29 19:21:31 +0800

    11_29

--- 二叉树相关/二叉树.h
+++ 二叉树相关/二叉树.h
@@ -39,6 +39,11 @@ public:
 		size_t index = 0;
 		_root = _CreateBinaryTree(a, size, index, invaild);
 	}
+	//重建二叉树
+	void RebuildBinaryTree(T *preorder, T*infixorder,size_t lengh)
+	{
+		_root = _RebuildBinaryTree(preorder, infixorder, lengh);
+	}
 	//析构函数
 	~BinaryTree()
 	{
@@ -71,8 +76,16 @@ public:
 	std::swap(tmp._root,_root);
 	}
 	return *this;
-
 	}*/
+	
+
+	//查找两个节点最近的公共节点
+
+	//思路是：
+	Node* FindTwoCommonNode(Node* p1, Node*p2)
+	{
+		return _FindTwoCommonNode(_root, p1, p2);
+	}
 	//前序遍历
 	void PreorderPrint()
 	{
@@ -95,7 +108,7 @@ public:
 
 	}
 	//查找
-	Node* Find(Node* _root, const T& x)
+	Node* Find(const T& x)
 	{
 		return _Find(_root, x);
 
@@ -149,6 +162,7 @@ public:
 				s.pop();
 				cout << top->_value << " ";
 
+				cur = top->_right;
 			}
 		}
 		cout << endl;
@@ -158,7 +172,7 @@ public:
 	{
 		if (_root == NULL)
 		{
-			retur n;
+			return;
 		}
 		else
 		{
@@ -207,13 +221,48 @@ public:
 	//输出k层的节点个数
 	size_t GetKLevel(const size_t & k)
 	{
-		return _GetKLevel(_root,k);
+		return _GetKLevel(_root, k);
 	}
 	//输出叶子节点的个数
 	size_t Leafsize()
 	{
 		return _Leafsize(_root);
 	}
+	//判断二叉是是否是完全二叉树
+
+	bool IsComplateBinaryTree()
+	{
+		queue<Node *> q;
+		Node* cur = _root;
+		q.push(cur);
+		//首先进行层序，把每一层的节点都入队，这个时候不存在的节点就按照NULL入队
+		while (（cur=q.pop()）！=NULL)
+		{
+
+			q.push(cur->_left);
+			q.push(cur->_right);
+		}
+		//这个时候如果是完全二叉树，那么队中就全部为空，如果为非完全二叉树的话，那么队中就会存在不为空的元素。
+		while (!q.empty())
+		{
+			cur = q.pop();
+			if (cur != NULL)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	//求得二叉树中最远两个节点之间的距离
+	size_t MaxDistance()
+	{
+		size_t maxdistance = 0;
+		return  _MaxDistance(_root,maxdistance);
+
+	}
+
+
 protected:
 	//深度
 	size_t _Depth(Node *root)
@@ -237,8 +286,8 @@ protected:
 		{
 			return 1;
 		}
-		
-		return _Leafsize(root->_left)+_Leafsize(root->_right);
+
+		return _Leafsize(root->_left) + _Leafsize(root->_right);
 	}
 	//查找结点
 	Node* _Find(Node *root, const T& x)
@@ -260,10 +309,11 @@ protected:
 		return cur;
 
 	}
-	//层序遍历
+	
+	//使用队列//层序遍历
 	void _LevelPrint(Node* root)
 	{
-		queue<Node *> q;
+		queue q;			//实现的队列
 		q.push(root);
 		while (!q.empty())
 		{
@@ -282,20 +332,20 @@ protected:
 		}
 	}
 	//得到K层的节点个数
-	size_t _GetKLevel(Node* root,size_t k)
+	size_t _GetKLevel(Node* root, size_t k)
 	{
-		if (root == NULL||k==0)		//考虑为空
+		if (root == NULL || k == 0)		//考虑为空
 		{
 			return 0;
 		}
-		else if (k==1)				//考虑第一层只有一个节点，不需要进行递归
+		else if (k == 1)				//考虑第一层只有一个节点，不需要进行递归
 		{
 			return 1;
 		}
 		else
 		{
 			//递归，
-			return _GetKLevel(root->_left, k - 1) + _GetKLevel(root->_right, k - 1);  
+			return _GetKLevel(root->_left, k - 1) + _GetKLevel(root->_right, k - 1);
 		}
 
 	}
@@ -316,7 +366,7 @@ protected:
 	{
 		if (root == NULL)
 			return 0;
-		return _size(root->_left)+_size(root->_right)+1;
+		return _size(root->_left) + _size(root->_right) + 1;
 	}
 	//创建二叉树
 	Node * _CreateBinaryTree(T *a, size_t size, size_t &index, const T& invaild)
@@ -374,6 +424,80 @@ protected:
 			cout << root->_value << " ";
 		}
 	}
+
+	//查找最近的两个公共节点
+	Node* _FindTwoCommonNode(Node* root, Node* p1, Node *p2)
+	{
+		if (root == NULL)
+		{
+			return NULL;
+		}
+		//判断是否找到这个节点
+		if (root == p1 || root == p2)
+		{
+			return root;
+		}
+		//递归右树和左树
+		Node* left = _FindTwoCommonNode(root->_left, p1, p2);
+		Node* right = _FindTwoCommonNode(root->_right, p1, p2);
+		//判断是否在左子树和右子树中找到
+		if (left&&right)
+			//左右子树都存在就返回root，此时的root就是公共节点
+			return root;	
+		//否则返回找到的一个
+		return left ? left : right;
+	
+	}
+
+	//求二叉树中最远的两个节点的距离
+	size_t _MaxDistance(Node *root,size_t &depth)
+	{
+		if (root == NULL)
+		{
+			depth = 0;
+			return 0;
+		}
+		size_t leftdepth = 0;
+		size_t rightdepth = 0;
+		size_t maxleft = _MaxDistance(root->_left, leftdepth);
+		size_t maxright = _MaxDistance(root->_right, rightdepth);
+		depth = max(leftdepth, rightdepth) + 1;
+		return max(maxleft, max(maxright, leftdepth + rightdepth));
+
+	}
+
+
+	//重建二叉树
+
+	//通过递归进行控制重建二叉树
+	Node* _RebuildBinaryTree(T* preorder, T* infixorder, int lengh)
+	{
+		if (lengh == 0 || preorder == NULL || infixorder == NULL)
+		{
+			return NULL;
+		}
+		//首先给根
+		T tmp = preorder[0];
+		//创建节点
+		Node *root = new Node(tmp);
+		//找到对应的在中序序列当中的所对应的根节点位置
+		int i = 0;
+		for (i = 0; i < lengh&&infixorder[i] != tmp; i++)
+			;
+		//得到左子树的长度
+		int leftlen = i;
+		//得到右子树的长度
+		int rightlen = lengh - i - 1;
+		if (leftlen>0)
+			//递归问题
+			root->_left = _RebuildBinaryTree(preorder+1, infixorder, leftlen);
+		if (rightlen > 0)
+			//递归问题
+			root->_right = _RebuildBinaryTree(preorder+leftlen + 1, infixorder+leftlen + 1, rightlen);
+
+		return root;
+	}
+
 protected:
 	Node* _root;
 
--- 二叉树相关/搜索二叉树.h
+++ 二叉树相关/搜索二叉树.h
@@ -5,10 +5,10 @@
 #include<cassert>
 using namespace std;
 
-template<typename K,typename V>
+template<typename K, typename V>
 struct SearchBinaryTreeNode
 {
-	typedef SearchBinaryTreeNode<K,V> Node;
+	typedef SearchBinaryTreeNode<K, V> Node;
 	SearchBinaryTreeNode()
 		:_left(NULL)
 		, _right(NULL)
@@ -22,7 +22,7 @@ struct SearchBinaryTreeNode
 	{
 	}
 
-	
+
 	V _value;
 	Node* _left;
 	Node* _right;
@@ -30,11 +30,11 @@ struct SearchBinaryTreeNode
 
 };
 
-template<typename K,typename V>
+template<typename K, typename V>
 class SearchBinaryTree
 {
 protected:
-	typedef SearchBinaryTreeNode<K,V>  Node;
+	typedef SearchBinaryTreeNode<K, V>  Node;
 
 public:
 	SearchBinaryTree(const K* arr, size_t size)
@@ -44,17 +44,72 @@ public:
 		{
 			InsertP(arr[i]);
 		}
-	
+
 	}
 	~SearchBinaryTree()
 	{
-		_root=_DestorySBT(_root);
+		_root = _DestorySBT(_root);
 
 	}
+
+	//查找最近的两个公共节点
+	Node *FindTwoCommonNode(Node* p1,Node* p2)
+	{
+		K minkey = 0;
+		K maxkey = 0;
+		if (p1->_key > p2->_key)
+		{
+			maxkey = p1->_key;
+			minkey = p2->_key;
+		}
+		else
+		{
+			maxkey = p2->_key;
+			minkey = p1->_key;
+		}
+		Node* cur = _root;
+		while (cur)
+		{
+			//考虑两类情况，一种是在符合的节点
+			if (cur->_key >= minkey&&cur->_key <= maxkey)
+			{
+				return cur;
+			}
+			//此时cur所指向节点比两个节点都大。所以两个节点都在左子树
+			else if (cur->_key > minkey&&cur->_key > maxkey)
+			{
+				cur = cur->_left;
+			}
+			//对比上一步
+			else
+			{
+				cur = cur->_right;
+			}
+
+		}
+
+		return NULL;
+	}
+
+	//BST转换排序的双向链表
+	Node* CovertSortList()
+	{
+		if (_root == NULL)
+		{
+			return NULL;
+		}
+		Node* newlist = NULL;
+		Node* prenode = NULL;
+		newlist = _CovertSortList(_root,prenode);
+
+		return newlist;
+	}
+	//递归插入
 	bool InsertP(const K& key)
 	{
 		return _InsertP(_root, key);
 	}
+	//非递归插入
 	bool Insert(const K& key)
 	{
 		if (_root == NULL)
@@ -93,20 +148,22 @@ public:
 		}
 		return true;
 	}
+	//递归删除
 	bool RemoveP(const K& key)
 	{
 		return _RemoveP(_root, key);
 	}
+	//非递归删除
 	bool Remove(const K& key)
 	{
 		Node *cur = _root;
 		Node *parent = NULL;
 		Node *del = NULL;
-		while (cur&&cur->_key!=key)
+		while (cur&&cur->_key != key)
 		{
 			if (cur->_key < key)
 			{
-				
+
 				parent = cur;
 				cur = cur->_right;
 			}
@@ -123,7 +180,7 @@ public:
 		//没找到进行处理
 		if (cur == NULL)
 		{
-			return false;	
+			return false;
 		}
 		//对左为空进行处理
 		if (cur->left == NULL)
@@ -148,7 +205,7 @@ public:
 			}
 		}
 		//对右为空进行处理
-		else if (cur->_right==NULL)
+		else if (cur->_right == NULL)
 		{
 			del = cur;
 			if (parent == NULL)
@@ -166,7 +223,7 @@ public:
 					parent->_right = cur->_right;
 				}
 			}
-		
+
 		}
 		//两个都不为空进行处理
 		else
@@ -190,45 +247,51 @@ public:
 		del = NULL;
 		return true;
 	}
+	//中序打印
 	void Inorderprint()
 	{
 		_Inorderprint(_root);
 		cout << endl;
 	}
+	//判断BST是否为空
 	bool Empty()
 	{
 		return _root == NULL;
 	}
-	bool Find(const K &key)
+	//查找结点
+	Node* Find(const K &key)
 	{
 		if (_root == NULL)
-			return false;
+			return NULL;
 		Node *cur = _root;
 		while (cur)
 		{
 			if (key > cur->_key)
 			{
-				cur = cur->_left;
+				cur = cur->_right;
 			}
 			else if (key < cur->_key)
 			{
-				cur = cur->_right;
+				cur = cur->_left;
 			}
 			else
 			{
-				return true;
+				return cur;
 			}
 		}
-		return false;
+		return NULL;
 	}
+	//递归查找结点
 	bool FindP(const K &key)
 	{
 		return _FindP(_root, key);
 	}
+	//返回BST根的键值
 	const K& Top()
 	{
 		return _root->_key;
 	}
+	//返回BST最小值
 	const K& Minvalue()
 	{
 		assert(_root);
@@ -239,6 +302,7 @@ public:
 		}
 		return cur->_value;
 	}
+	//返回BST最大值
 	const K& Maxvalue()
 	{
 		assert(_root);
@@ -250,8 +314,12 @@ public:
 		return cur->_value;
 	}
 protected:
-	Node* _DestorySBT(Node *root)
+	Node* _DestorySBT(Node *&root)
 	{
+		if (root == NULL)
+		{
+			return NULL;
+		}
 		if (root != NULL)
 		{
 			root->_left = _DestorySBT(root->_left);
@@ -266,7 +334,7 @@ protected:
 		if (root == NULL)
 			return;
 		_Inorderprint(root->_left);
-		cout << root->_key <<" ";
+		cout << root->_key << " ";
 		_Inorderprint(root->_right);
 	}
 	bool _InsertP(Node *&root, const K& key)
@@ -298,11 +366,11 @@ protected:
 		}
 		if (root->_key < key)
 		{
-			_RemoveP(root->_right,key);
+			_RemoveP(root->_right, key);
 		}
 		else if (root->_key > key)
 		{
-			_RemoveP(root->_left,key);
+			_RemoveP(root->_left, key);
 		}
 		else
 		{
@@ -319,7 +387,7 @@ protected:
 			}
 			else
 			{
-			
+
 				Node* parent = root;
 				Node *swapnode = root->_right;
 				while (swapnode->_left)
@@ -340,7 +408,7 @@ protected:
 		}
 		return false;
 	}
-	bool _FindP(Node * root,const K& key)
+	bool _FindP(Node * root, const K& key)
 	{
 		if (root == NULL)
 		{
@@ -359,6 +427,29 @@ protected:
 			return true;
 		}
 		return false;
+	}
+
+	//BST转换排序的双向链表
+	Node* _CovertSortList(Node *root,Node *&prenode)
+	{
+		if (root==NULL)
+			return NULL;
+		Node* cur = root;
+
+		//先要找到BST的最小键值点，也就是最左节点。寻找最左节点的时候切记prenode是不能变的。
+		if (root->_left)
+			_CovertSortList(cur->_left, prenode);
+		//找到以后，这个最左节点的左边链上prenode
+		cur->_left = prenode;
+		//这个时候考虑给前一个节点的右链上节点
+		if (prenode != NULL)
+			prenode->_right = cur;
+		//改变前一个节点为当前节点
+		prenode = cur;
+		//考虑右子树的问题
+		if (cur->_right)
+			_CovertSortList(cur->_right,prenode);
+		return root;
 	}
 protected:
 	Node * _root;
